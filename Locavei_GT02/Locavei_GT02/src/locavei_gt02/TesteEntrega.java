8188104a7ebec0f10c664f595ef31ea2f3fd80ba
AlineOP201 <alineveira2@gmail.com>
2024-04-30 22:55:36 -0300

    adicionando arquivo

---
+++ Locavei_GT02/Locavei_GT02/src/locavei_gt02/TesteEntrega.java
@@ -0,0 +1,19 @@
+
+package locavei_gt02;
+
+/**
+ *
+ * @author 364975
+ */
+public class TesteEntrega {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Entrega ent = new Entrega();
+        ent.cadastro();
+        ent.mostra();
+    }
+    
+}
---
+++ Locavei_GT02/Locavei_GT02/src/locavei_gt02/df.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package locavei_gt02;
+
+/**
+ *
+ * @author 364975
+ */
+class df {
+    
+}
