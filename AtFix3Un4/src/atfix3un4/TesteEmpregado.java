20730a6f1bb51990b70bf1e71dfbffa50202e7ef
alineop120 <alineveira2@gmail.com>
2024-05-21 16:57:33 -0300

    salvando alterações da classe TesteEmpregado

--- AtFix3Un4/src/atfix3un4/TesteEmpregado.java
+++ AtFix3Un4/src/atfix3un4/TesteEmpregado.java
@@ -5,6 +5,8 @@
  */
 package atfix3un4;
 
+import java.util.Scanner;
+
 /**
  * Instituição          Escola Técnica de Brasília - ETB
  * Nome do Curso        Curso Técnico de Informática
@@ -20,10 +22,34 @@ public class TesteEmpregado {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Empregado emp = new Empregado("");
-	emp.recebe();
-	emp.calcIR();
-	emp.mostra();
+        boolean loop = true;
+        Scanner sc = new Scanner(System.in);
+        while (loop){
+            // Menu
+            System.out.println("MENU -->");
+            System.out.println("1] Programador");
+            System.out.println("2] Executivo");
+            System.out.println("3] Encerrar");
+            int op = sc.nextInt();
+            switch (op){
+                case 1: 
+                    Programador pr = new Programador("", 0, 0);
+                    pr.recebe();
+                    pr.calcIR();
+                    pr.calcAdicional();
+                
+                case 2: 
+                    Executivo ex = new Executivo("", 0);
+                    ex.calcIR();
+                    ex.calcProducao(.2f);
+                    ex.mostra();
+                
+                case 3: 
+                    System.out.println("Programa encerado!");
+                    System.exit(0);
+                default: 
+                    System.out.println("Opção errada, voltando para o menu!");
+            }
+        }
     }
-    
 }
