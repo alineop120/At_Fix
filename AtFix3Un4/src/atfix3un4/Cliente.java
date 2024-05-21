cf0ff70c0ecb4eb7779eaee780e26843d0d70525
ALINE OLIVEIRA DE PINHO <364975@etb.net.br>
2024-05-21 14:30:38 -0300

    movend arquivo para a pasta AtFix3Un4

---
+++ AtFix3Un4/src/atfix3un4/Cliente.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atfix3;
+
+/**
+ *
+ * @author 364975
+ */
+public class Cliente {
+    // Atributos
+    private String nomeCli;
+    private float totDep;
+    
+}
